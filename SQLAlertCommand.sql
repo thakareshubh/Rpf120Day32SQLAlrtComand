73517e7a56ed281ae9322892b892abbb3c653a0c
thakareshubh <shubhamthakare032@gmail.com>
2022-04-08 17:17:37 +0530

    Added column into table

--- SQLAlertCommand.sql
+++ SQLAlertCommand.sql
@@ -1,2 +1,17 @@
 use payroll_service
 select * from employee_payroll
+
+Alter table  employee_payroll
+Add Basic_Pay Money;
+
+Alter table employee_payroll
+Add Deduction Money;
+
+Alter table employee_payroll
+Add Taxable_Pay Money; 
+
+Alter table employee_payroll
+Add Income_Tax Money;
+
+Alter table employee_payroll
+Add Net_Pay Money;
